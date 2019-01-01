8cb9dfbd71de72911020d309168de4dc06e5ff0f
Kaden Nelson <kadenlnelson@gmail.com>
2019-01-01 09:47:55 -0700

    updated docker files

--- scripts/init/installation.sh
+++ scripts/init/installation.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
-docker build -t dhl-experimental . && echo "copying files over..."&& docker run --rm dhl-experimental tar -cC /var/www/html . | tar -xC ./
+echo "removing previously existing webroot directory"
+rm -rf ./webroot && mkdir ./webroot
+
+docker build -t dhl-experimental .
+
+echo "copying files over..."
+docker run --rm dhl-experimental tar -cC /var/www/html . | tar -xC ./webroot
+echo "file transfer completed!"
 
 if [ "$1" == "--start" ]; then
   echo "Starting docker-headless-lightning development orchestration"
