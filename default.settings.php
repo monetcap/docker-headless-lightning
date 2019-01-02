f3ba5c7753255638f8f325c3d24f634c03841cbb
Kaden Nelson <kadenlnelson@gmail.com>
2019-01-02 14:27:55 -0700

    updated installation.sh

--- default.settings.php
+++ default.settings.php
@@ -5,3 +5,10 @@ if (file_exists('/tmp/mysql.env')) {
 }
 
 $config_directories[CONFIG_SYNC_DIRECTORY] = "../config";
+
+$settings['trusted_host_patterns'] = array(
+    '^example\.com$',
+    '^.+\.example\.com$',
+    '^127\.0\.0\.1$',
+    '^localhost$',
+);
