606cd163200c9803aa4224d967907d6b189c494a
Kaden Nelson <kadenlnelson@gmail.com>
2019-01-02 09:49:30 -0700

    whoops forgot to add this file

---
+++ database.settings.php
@@ -0,0 +1,10 @@
+$databases['default']['default'] = array (
+    'database' =>  getenv('MYSQL_DATABASE'),
+    'username' => getenv('MYSQL_USER'),
+    'password' => getenv('MYSQL_PASSWORD'),
+    'prefix' => '',
+    'host' => getenv('MYSQL_HOST'),
+    'port' => getenv('MYSQL_PORT'),
+    'namespace' => 'Drupal\\Core\\Database\\Driver\\mysql',
+    'driver' => 'mysql',
+);
