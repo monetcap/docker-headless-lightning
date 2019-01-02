d60577b6a9bc754a78826769c92d06fac963a9d3
Kaden Nelson <kadenlnelson@gmail.com>
2019-01-01 20:54:11 -0700

    updated default.settings.php to include trusted_host_patterns

--- default.settings.php
+++ default.settings.php
@@ -1,1 +1,8 @@
+$settings['trusted_host_patterns'] = array(
+    '^example\.com$',
+    '^.+\.example\.com$',
+    '^127\.0\.0\.1$',
+    '^localhost$',
+);
+
 $config_directories[CONFIG_SYNC_DIRECTORY] = "../config";
